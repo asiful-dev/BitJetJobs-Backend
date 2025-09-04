815a10e3745360068ae63aa7aee978533c42f095
AwfulAsiful <awful872@gmail.com>
2025-09-04 19:51:30 +0600

    created DTO's for different user roles job-seeker, employer

---
+++ src/users/dto/employer-profile.dto.ts
@@ -0,0 +1,14 @@
+import { IsEnum, IsString,IsArray } from 'class-validator';
+import { NumJobs, JobRoles } from 'generated/prisma';
+
+export class EmployerProfileDto {
+  @IsString()
+  companyName: string;
+
+  @IsEnum(NumJobs)
+  numJobs: NumJobs;
+
+  @IsArray()
+  @IsEnum(JobRoles, { each: true })
+  jobRoles: JobRoles[];
+}
---
+++ src/users/dto/job-seeker-profile.dto.ts
@@ -0,0 +1,19 @@
+import { IsEnum, IsString, IsArray, IsOptional } from 'class-validator';
+import { JobSeekerRole, WorkType } from 'generated/prisma';
+
+export class JobSeekerProfileDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsArray()
+  @IsEnum(JobSeekerRole, { each: true })
+  roles: JobSeekerRole[];
+
+  @IsArray()
+  @IsEnum(WorkType, { each: true })
+  preferredWorkTypes: WorkType[];
+}
