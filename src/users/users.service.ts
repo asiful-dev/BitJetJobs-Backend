11aa826c9e64c588e86c5a2e5dda89661b5dbdf9
AwfulAsiful <awful872@gmail.com>
2025-09-04 19:55:46 +0600

    Users Service with Profile Update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Prisma } from 'generated/prisma';
+import { Prisma, UserRole } from 'generated/prisma';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
@@ -34,5 +34,28 @@ export class UsersService {
     return this.databaseService.user.delete({
       where: { id },
     });
+  }
+  async updateProfile(id: number, role: UserRole, profileData: any) {
+    if (role === UserRole.JOB_SEEKER) {
+      return this.databaseService.jobSeekerProfile.upsert({
+        where: { userId: id },
+        update: { ...profileData },
+        create: {
+          userId: id,
+          ...profileData,
+        },
+      });
+    } else if (role === UserRole.EMPLOYER) {
+      return this.databaseService.employerProfile.upsert({
+        where: { userId: id },
+        update: { ...profileData },
+        create: {
+          userId: id,
+          ...profileData,
+        },
+      });
+    }
+
+    throw new BadRequestException('Invalid user role.');
   }
 }
